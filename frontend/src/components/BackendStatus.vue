4eff168cd1164a99dca4b8e8e17e5e386f999ce1
Mikael Kalms <mikael@kalms.org>
2023-12-27 18:16:07 +0100

    Display backend status on dashboard

---
+++ frontend/src/components/BackendStatus.vue
@@ -0,0 +1,20 @@
+<template>
+  Backend status: 
+  <template v-if="props.status === true">
+    Up
+  </template>
+  <template v-else-if="props.status === false">
+    Down
+  </template>
+  <template v-else>
+    Unknown
+  </template>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+  status: boolean|undefined;
+}>();
+
+</script>
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,7 +1,39 @@
 <template>
   <h2>Dashboard</h2>
+  <BackendStatus :status="backendStatus"/>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+import { api } from '../backendApi';
+import { HttpStatusCode } from 'axios';
 
+import BackendStatus from '../components/BackendStatus.vue';
+
+const backendStatus = ref(undefined as boolean|undefined);
+
+const BACKEND_STATUS_REFRESH_INTERVAL = 1*1000;
+
+let backendStatusPollId: ReturnType<typeof setInterval>|undefined = undefined;
+
+onMounted(() => {
+
+  backendStatusPollId = setInterval(async () => {
+    try {
+      const result = await api.health();
+      if (result.status == HttpStatusCode.Ok) {
+        backendStatus.value = true;
+      } else {
+        backendStatus.value = false;
+      }
+    } catch (error) {
+      backendStatus.value = false;
+    }
+  }, BACKEND_STATUS_REFRESH_INTERVAL);
+});
+
+onUnmounted(() => {
+  clearInterval(backendStatusPollId);
+  backendStatusPollId = undefined;
+});
 </script>
